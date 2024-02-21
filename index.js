9d481ed73e8d2c62285ab16b1b3466adc31b9758
lokesh <lokesh.malviya@edgistify.com>
2024-02-21 10:53:54 +0530

    test cicd

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express();
 const port = process.env.PORT | 8000;
 
 app.get("/", (req, res) => {
-  res.send(`Hello from port ${port}!`);
+  res.sendStatus(200).json({
+    status: success,
+    response: `Success`,
+    port: port,
+  });
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
