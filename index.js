8f991cff84ae19ea2c06a949344178be4e3840d9
lokesh <lokesh.malviya@edgistify.com>
2024-02-21 17:11:30 +0530

    api rout

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ const app = express();
 
 const port = process.env.PORT | 8000;
 
-app.get("/", (req, res) => {
+app.get("/api/users", (req, res) => {
   res.json({
     status: "success",
-    response: `Success`,
-    port: port,
+    user: {
+      _id: "5ce45d7606444f199acfba1e",
+      name: { given: "Alex", family: "Smith" },
+      email: "email@example.com",
+      age: 27,
+    },
+    response_from_port: port,
+  });
+});
+
+app.get("/api/products", (req, res) => {
+  res.json({
+    status: "success",
+    user: {
+      _id: "5ce45d7606444f199acfba2e",
+      name: "One plus",
+      price: 27,
+    },
+    response_from_port: port,
   });
 });
 
